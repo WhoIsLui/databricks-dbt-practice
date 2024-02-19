7f33143c1b5d10c99ba4a3a23aa3a9bd70240840
Lui? <63835043+WhoIsLui@users.noreply.github.com>
2024-02-18 21:03:28 -0300

    test

--- dbt_practice_job/models/example/python_model.py
+++ dbt_practice_job/models/example/python_model.py
@@ -2,6 +2,6 @@
 
 def model(dbt, session):
     print(__name__)
-    return dbt.utils.execute_query(
-        "SELECT * FROM example_table"
-    )
+    df = dbt.ref('my_first_dbt_model')
+    df = df.withColumn('new_column', 1)
+    return df
